886779378c4f0ada9a870a1c359c44e12fadf40b
nbyarley <nbyarley@fullsail.edu>
2013-05-20 22:21:20 -0400

    many new additions
    Added boolean function that returns the boolean. created dogs object
    with a function within the object. created the outputs for the pbject
    and functions. also revamped the information within my json file.

--- dogs.js
+++ dogs.js
@@ -7,18 +7,12 @@
 //global Variables
 var petShop = "Dogs Haven";
 var dogName = "\"Benny\"";
+var dogShots = true;
+var dogGroom = true;
+var grooming;
 var ladyConversationOne = "Hello Sir, how may i help you? ";
 var ladyConversationTwo = "Great we have a nice selection of pure breed dogs to chose from. ";
 
-///////////////////////////////// Start Objects ///////////////////////////////////
-
-var dogs = {
-	dogBreed: ["Beagle", "Mastiff", "Bulldog", "Pitbull", "Collie"],
-	dogBreedAmount: [2, 4, 2, 1, 1],
-	dogAge: [3, 7, 1, 2, 2],
-};
-
-///////////////////////////////// End Objects ///////////////////////////////////
 
 ///////////////////////////////// Start Functions ///////////////////////////////////
 
@@ -35,13 +29,43 @@ var talkingToLady = function(conversationOne, conversationTwo) {
 var dogList = function (dogListJson) {
 	for (var i = 0; i < dogListJson.dogs.length; i++) {
 		var dogs = dogListJson.dogs[i];
-		console.log("Dog Breed: " + dogs.dogBreed + ", \nAge: " + dogs.dogAge + ", \nwe currently have " + dogs.dogBreedAmount + " of this breed");
+		console.log("Dog Breed: " + dogs.dogBreed + ", \nAge: " + dogs.dogAge + ", \nwe currently have " + dogs.dogBreedAmount + " of this breed \nPrice: $" + dogs.dogPrice);
 		console.log(" -------------------------------- ");
 	}
 };
 
+//do the dogs have their shots
+//pull information from dogs object
+var dogsHealthy = function(dogGroom, dogShots) {
+	var statement;
+	
+	if (dogGroom === true && dogShots === true) {
+		console.log("Yes the dogs are up to date on all their shots. ");
+		statement = true;
+			
+	} else {
+		console.log("I am sorry, the dogs are not up to date on their shots. ");
+		statement = true;
+	}
+	return statement;
+};
+
 
 ///////////////////////////////// End Functions ///////////////////////////////////
+
+///////////////////////////////// Start Objects ///////////////////////////////////
+
+var dogs = {
+	dogBreed: ["Beagle", "\nMastiff", "\nBulldog"],
+	totalBreed: ["Beagle", "Mastiff", "Bulldog", "Pitbull", "Collie"],
+	dogBreedAmount: [2, 4, 2, 1, 1],
+	
+	grooming: function(groomed) {
+		console.log("The following list of dogs are unavailable due to being groomed\n" + groomed);
+	}
+}
+
+///////////////////////////////// End Objects ///////////////////////////////////
 
 
 ///////////////////////////////// Start Output ///////////////////////////////////
@@ -54,5 +78,17 @@ console.log(" "); //spacer
 //Json Data output
 console.log("Here is our current Dog Listing: ");
 dogList(dogListJson)
+
+console.log(" "); //spacer
+
+//calling function dogsHealthy
+console.log("Have the dogs had their shots up to date?");
+
+dogsHealthy(dogGroom, dogShots)
+console.log(" "); //spacer
+
+//calling object function
+//list of dogs being groomed
+dogs.grooming(dogs.dogBreed);
 
 ///////////////////////////////// End Output ///////////////////////////////////
--- json.js
+++ json.js
@@ -9,27 +9,32 @@ var dogListJson = {
 	{
 		"dogBreed": "Beagle",
 		"dogAge": 3,
-		"dogBreedAmount": 2
+		"dogBreedAmount": 2,
+		"dogPrice": 250
 	},
 	{
 		"dogBreed": "Mastiff",
 		"dogAge": 7,
-		"dogBreedAmount": 4
+		"dogBreedAmount": 4,
+		"dogPrice": 400
 	},
 	{
 		"dogBreed": "Bulldog",
 		"dogAge": 1,
-		"dogBreedAmount": 2
+		"dogBreedAmount": 2,
+		"dogPrice": 350
 	},
 	{
 		"dogBreed": "Pitbull",
 		"dogAge": 2,
-		"dogBreedAmount": 1
+		"dogBreedAmount": 1,
+		"dogPrice": 650
 	},
 	{
 		"dogBreed": "Collie",
 		"dogAge": 2,
-		"dogBreedAmount": 1
+		"dogBreedAmount": 1,
+		"dogPrice": 420
 	}
 	]
-};
+}; 
